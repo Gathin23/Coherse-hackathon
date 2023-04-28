593e2286dbc63f905c9f41e3eb0a35975e936d8f
Gathin <gathint23@gmail.com>
2023-04-29 00:42:12 +0530

    Spatial embed

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe className="space" src="https://www.spatial.io/s/GATHINs-Lo-Fi-World-6436b417c5837fcb1d1c2ad8?share=4677200397325018113" allow="microphone https://spatial.io; camera https://spatial.io"></iframe>
+    </div>
+  );
+}
+
+export default App;
